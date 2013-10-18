21dcdc0a54650ca212159299bf16985291e220e9
eboning <eboning@ucsc.edu>
2013-10-18 13:17:02 -0700

    Added Actions

---
+++ src/edu/ucsc/gameAI/GoUpAction.java
@@ -0,0 +1,19 @@
+package edu.ucsc.gameAI;
+
+import edu.ucsc.gameAI.decisionTrees.binary.IBinaryNode;
+import pacman.game.Constants.MOVE;
+import pacman.game.Game;
+
+public class GoUpAction implements IAction, IBinaryNode {
+
+	public void doAction() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public IAction makeDecision(Game game) {return this;}
+	
+	public MOVE getMove(){
+		return MOVE.UP;		
+	}
+}
